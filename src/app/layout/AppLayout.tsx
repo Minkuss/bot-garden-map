3496aeba799f67b57bbf53c4fa13f6a6b9316a2f
nikita <n.shiyan@thewhite.ru>
2025-11-04 23:43:31 +1000

    added footer

--- src/app/layout/AppLayout.tsx
+++ src/app/layout/AppLayout.tsx
@@ -1,10 +1,12 @@
 import { Container } from 'src/shared/ui/container/container';
 import { Header } from 'src/widgets/header/ui/header';
 import { Outlet } from 'react-router-dom';
+import { Footer } from 'src/widgets/footer/ui/footer';
 
 export const AppLayout = () => (
     <Container>
         <Header/>
         <Outlet/>
+        <Footer/>
     </Container>
 );
