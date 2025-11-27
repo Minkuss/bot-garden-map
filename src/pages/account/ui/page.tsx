8a851a6dcb0d1790b7b5c8a0c01bb5f22bf10543
nikita <n.shiyan@thewhite.ru>
2025-11-27 23:12:25 +1000

    account page animation

--- src/pages/account/ui/page.tsx
+++ src/pages/account/ui/page.tsx
@@ -4,9 +4,25 @@ import { AccountBillboardsInfo } from 'src/widgets/accountBillboardsInfo/ui/acco
 import { useAuth } from 'src/shared/auth/hooks/useAuth';
 import { Navigate } from 'react-router-dom';
 import { routes } from 'src/shared/routes';
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 export const AccountPage = () => {
     const { user } = useAuth();
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    useGSAP(() => {
+        gsap.fromTo(containerRef.current, {
+            y: '50%',
+            opacity: 0,
+        }, {
+            opacity: 1,
+            y: 0,
+            duration: 1.5,
+            ease: 'power3.out',
+        });
+    });
 
     if (!user) {
         return <Navigate to={routes.MAP} replace={true}/>;
@@ -18,8 +34,17 @@ export const AccountPage = () => {
                 scrollSnapAlign: 'start',
             }}
         >
-            <AccountUserInfo/>
-            <AccountBillboardsInfo/>
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '40px',
+                }}
+                ref={containerRef}
+            >
+                <AccountUserInfo/>
+                <AccountBillboardsInfo/>
+            </div>
         </Container>
     );
 };
