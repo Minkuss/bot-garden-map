40c7ff3ef018ce99c4f24835792f6cf9b17da267
nikita <n.shiyan@thewhite.ru>
2025-11-27 22:59:18 +1000

    little fixes

--- src/pages/account/ui/page.tsx
+++ src/pages/account/ui/page.tsx
@@ -1,14 +1,25 @@
 import { AccountUserInfo } from 'src/widgets/accountUserInfo/ui/accountUserInfo';
 import { Container } from 'src/shared/ui/container/container';
 import { AccountBillboardsInfo } from 'src/widgets/accountBillboardsInfo/ui/accountBillboardsInfo';
+import { useAuth } from 'src/shared/auth/hooks/useAuth';
+import { Navigate } from 'react-router-dom';
+import { routes } from 'src/shared/routes';
 
-export const AccountPage = () => (
-    <Container
-        style={{
-            scrollSnapAlign: 'start',
-        }}
-    >
-        <AccountUserInfo/>
-        <AccountBillboardsInfo/>
-    </Container>
-);
+export const AccountPage = () => {
+    const { user } = useAuth();
+
+    if (!user) {
+        return <Navigate to={routes.MAP} replace={true}/>;
+    }
+
+    return (
+        <Container
+            style={{
+                scrollSnapAlign: 'start',
+            }}
+        >
+            <AccountUserInfo/>
+            <AccountBillboardsInfo/>
+        </Container>
+    );
+};
